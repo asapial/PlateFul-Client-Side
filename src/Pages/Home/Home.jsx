1ff25ae4047d092a50e4cf4c6353b744ec14a6b3
Abu Syeed Abdullah <72955464+asapial@users.noreply.github.com>
2025-05-22 02:53:17 +0600

    slider image is added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import TopRecipes from '../../Components/recipe/TopRecipes';
 import Lottie from 'lottie-react';
 import groovyWalkAnimation from "/src/assets/walking.json";
+import Slider from '../../Components/common/Slider';
 
 const Home = () => {
     const topRecipeDataPromise= fetch('http://localhost:3000/topRecipe').then(res=>res.json())
     console.log(topRecipeDataPromise);
     return (
         <div className=''>
+            <Slider></Slider>
             <TopRecipes topRecipeDataPromise={topRecipeDataPromise}></TopRecipes>
             <Lottie animationData={groovyWalkAnimation} className=' h-[400px]' />;
 
